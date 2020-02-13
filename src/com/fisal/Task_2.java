02bbbd35c5c6c7da66b3a616025593aeeeda0c93
TippuFisal <tippufisal@vajro.com>
2020-02-13 09:02:31 +0530

    Task_2

---
+++ src/com/fisal/Task_2.java
@@ -0,0 +1,25 @@
+package com.fisal;
+
+/*
+13-Feb-2020
+
+Day 2 Challenge
+
+Alex wants to learn Java but his computer's semicolan (;) button is not working.
+Can you help him to write his first java program while he fixes his computer ?
+
+*/
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+        //if conditon can count the value inside the bracket
+        //if its > 0 its true
+        //if its 0 then it call else part
+      if(System.out.printf("Printing Without Semicolan")==null){
+          //System.out.print("  done");
+      }else {
+          //System.out.print("  Fail");
+      }
+    }
+}
