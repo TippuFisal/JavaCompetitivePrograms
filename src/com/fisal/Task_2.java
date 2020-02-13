0c2a8e735765add7f8ba043bb5d38ac7da6d8e21
TippuFisal <tippufisal@vajro.com>
2020-02-13 09:54:20 +0530

    Task_2 (Printing withOut SemiColan) Adding Comments to UnderStand and Add Some Methods to Reference

--- src/com/fisal/Task_2.java
+++ src/com/fisal/Task_2.java
@@ -10,16 +10,45 @@ Can you help him to write his first java program while he fixes his computer ?
 
 */
 
+/*
+ * We can use format to print the statement
+ * System.out.format(" Super");
+ *
+ * */
+
 public class Task_2 {
 
     public static void main(String[] args) {
+        printfNullMethod();
+    }
+
+    //Method 1
+    public static void printfNullMethod(){
         //if conditon can count the value inside the bracket
         //if its > 0 its true
         //if its 0 then it call else part
-      if(System.out.printf("Printing Without Semicolan")==null){
-          //System.out.print("  done");
-      }else {
-          //System.out.print("  Fail");
-      }
+        if(System.out.printf("Printing Without Semicolan")==null){
+            //System.out.print("  done");
+        }else {
+            //System.out.print("  Fail");
+        }
+    }
+
+    //Method 2
+    public static void appendEqualsNullMethod(){
+        if(System.out.append("hey").equals(null)){
+            System.out.print("  done");
+        }else{
+            System.out.print("  Fail");
+        }
+    }
+
+    //Method 3
+    public static void appendEqualstooNullMethod(){
+        if(System.out.append("hey")== null){
+            System.out.print("  done");
+        }else{
+            System.out.print("  Fail");
+        }
     }
 }
