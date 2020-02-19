124c55b236bc633527b0b5e49ec2d491b36925a4
TippuFisal <tippufisal@vajro.com>
2020-02-20 00:15:04 +0530

    * Task 7 ---Answer Reference

--- src/com/fisal/Task/Task_7.java
+++ src/com/fisal/Task/Task_7.java
@@ -14,16 +14,22 @@ Slove without using *,/ operator, bitwise operator or loop.
 
 
 public class Task_7 {
-  static   long bus_people = 1200000;
+    static long bus_people = 1200000;
     static int year = 365;
     static int count;
-    static long value=0;
+    static long value = 0;
 
-    static int i=10;
-    static int j=1;
+    static int i = 10;
+    static int j = 1;
     static int k;
 
     public static void main(String[] args) {
+
+        int people = 1200000;
+        int year = 365;
+        int peopleYear= med(people,year);
+        System.out.println(peopleYear);
+
         method_2();
     }
 
@@ -34,14 +40,14 @@ public class Task_7 {
     *
     *
     **/
-    public static void med1(){
+    public static void med1() {
 
-        if(i>0){
-           i=i-j;
-           k++;
-           med1();
-        }else {
-            System.out.println("count "+k);
+        if (i > 0) {
+            i = i - j;
+            k++;
+            med1();
+        } else {
+            System.out.println("count " + k);
         }
     }
 
@@ -53,19 +59,19 @@ public class Task_7 {
      *
      *
      */
-    public static void method_1(){
-      try {
-          if(bus_people>0){
-              bus_people= bus_people-year;
-              count++;
-              method_1();
-          }else{
-              System.out.println(count);
-          }
-      }catch (Exception e){
-          System.out.println(e);
-          e.printStackTrace();
-      }
+    public static void method_1() {
+        try {
+            if (bus_people > 0) {
+                bus_people = bus_people - year;
+                count++;
+                method_1();
+            } else {
+                System.out.println(count);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -75,14 +81,30 @@ public class Task_7 {
     *
     *
     * */
-    public static void method_2(){
-        if(year>0){
-            value = bus_people+value;
+    public static void method_2() {
+        if (year > 0) {
+            value = bus_people + value;
             year--;
             method_2();
-        }else {
+        } else {
             System.out.println(value);
         }
     }
+
+    //reference
+    public static int med(int x, int y) {
+
+        if (y == 0) {
+
+        }
+        if (y > 0) {
+            return (x + med(x, y - 1));
+        }
+        if(y<0){
+            return -med(x,-y);
+        }
+        return -1;
+    }
+
 
 }
